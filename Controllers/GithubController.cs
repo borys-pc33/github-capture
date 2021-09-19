3e0847b21e03d03e5d51b00697e41785bf257a76
borysl <borys.lebeda@gmail.com>
2021-09-19 15:06:02 +0300

    Add sample with the webhook

---
+++ Controllers/GithubController.cs
@@ -0,0 +1,47 @@
+﻿using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Logging;
+
+namespace GithubWebhookCatcher.Controllers
+{
+    /// <summary>
+    /// Controller to accept webhooks from the github service.
+    /// </summary>
+    [ApiController]
+    [Route("[controller]")]
+    public class GithubController : ControllerBase
+    {
+        private readonly ILogger<GithubController> _logger;
+
+        public GithubController(ILogger<GithubController> logger)
+        {
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// Returns if the service is on.
+        /// </summary>
+        /// <returns>Status of the service.</returns>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var healthCheck = new HealthCheckResult(HealthStatus.Healthy);
+
+            return Ok(healthCheck);
+        }
+
+        /// <summary>
+        /// Accepts github.
+        /// </summary>
+        /// <param name="body">The body of the webhook.</param>
+        /// <returns>Always Ok result.</returns>
+        [HttpPost]
+        public IActionResult Post([FromBody] JsonElement body)
+        {
+            string json = System.Text.Json.JsonSerializer.Serialize(body);
+            System.Console.WriteLine($"Serialized: {json}");
+            return Ok();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,24 @@
+# Example to capture hooks
+
+## Prerequisites
+
+- .Net Core
+- node.js
+
+## Run
+
+```
+dotnet run
+```
+
+The swagger will be available at http://localhost:5000
+
+## Use ngrok as proxy
+
+### Install ngrok globally
+
+```
+npm i ngrok -g
+```
+
+### Run ngrok
